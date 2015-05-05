578759501f6c35928f3bd4ed7def8ca87e94e8e1
Alex Lal <TheGeneral@Alexs-MacBook-Pro-3.local>
2015-05-05 19:27:08 -0400

    Initial Commit

---
+++ Classy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Classy
+//
+//  Created by Alex Lal on 5/5/15.
+//  Copyright (c) 2015 Alex Lal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
